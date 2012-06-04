7c96a14dfb15bf7959d76b16c382adfef1808300
Torvald Lekvam <torvald@gmail.com>
2012-06-04 18:37:27 +0200

    added logs and apache module and some stuuuff :)

--- dailyreport.bash
+++ dailyreport.bash
@@ -6,10 +6,11 @@ source inc/functions.bash
 source inc/init.bash
 
 errorMsg=""
-
+echo "<pre>" > $MAIL
 MODULES=$(config modules)
 if [[ $MODULES == "modules not found in dailyreport.conf" ]]; then
-    MODULES="general" # Add all
+    MODULES=$(ls modules)
+    MODULES="${MODULES[@]}" # Add all
 fi
 for MODUL in $MODULES; do
     if [ ! -f modules/$MODUL ]; then
@@ -18,24 +19,31 @@ for MODUL in $MODULES; do
     fi
     modules/$MODUL > $TEMP # temp file from systemCheck
     if [ $? == 0 ]; then
-        cat $TEMP | sed 's/$/<br>/g' | sed 's/\t/\&emsp;/g' >> $MAIL
+        #cat $TEMP | sed 's/\t/\&emsp;/g' >> $MAIL
+        cat $TEMP >> $MAIL
         echo "<br><br>" >> $MAIL
     else
-        errorMsg=$errorMsg"<br> Module $MODUL returned with error signal<br>"
+        errorMsg=$errorMsg"Module $MODUL returned with error signal<br>"
     fi
 done
 
+
+
+DATE_YESTERDAY=$(date --date "1 day ago" +%d.%m.%Y)
 # appending any errors
 if [[ $errorMsg != "" ]]; then
+    errorMsg="<font color=\"red\">$errorMsg</font>"
     sed -i "1i$errorMsg" $MAIL
 fi
+
+echo "</pre>" >> $MAIL
 
 if [[ $1 == "test" ]]; then
     cat $MAIL
 else 
     RECIVERS=$(config recivers)
     for RECIVER in $RECIVERS; do
-        cat $MAIL | mail -a 'Content-Type: text/html' -s "Dailyreport test" $RECIVER
+        cat $MAIL | mail -a 'Content-Type: text/html' -s "Dailyreport for $HOSTNAME $DATE_YESTERDAY" $RECIVER
     done
 fi
 
